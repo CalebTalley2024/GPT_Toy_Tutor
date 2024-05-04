8a5e4b5b53a66b44642d9a75f69741a47dba69f3
Caleb Talley <calebtalley2024@gmail.com>
2024-05-04 17:46:15 -0400

    added MongoDB password as an environment variable

--- command_line_toy/database_connect.py
+++ command_line_toy/database_connect.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-password = "GPTTutor"
+load_dotenv()
+password = os.getenv('MONGODB_PASSWORD_1')
 
 uri = f"mongodb+srv://CalebTalley:{password}@cluster0.b3k8luy.mongodb.net/?retryWrites=true&w=majority" #TODO Add environment variables later
 # Create a new client and connect to the server
